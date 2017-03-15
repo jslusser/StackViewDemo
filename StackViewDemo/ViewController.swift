e9bbf162c667afdb6f70d040483fa91e30217539
jslusser <jslusser@gmail.com>
2017-03-15 19:04:19 +0800

    JCS StackViewDemo
    JCS StackViewDemo

--- StackViewDemo/ViewController.swift
+++ StackViewDemo/ViewController.swift
@@ -10,6 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cupStackView: UIStackView!
+    @IBAction func removeCup(_ sender: Any) {
+        let lastAddedCup = self.cupStackView.arrangedSubviews.last
+        
+        if let cup = lastAddedCup
+        {
+            UIView.animate(withDuration: 0.25,
+                           animations: {
+                            
+                            self.cupStackView.removeArrangedSubview(cup)
+                            cup.removeFromSuperview()
+                            self.cupStackView.layoutIfNeeded()
+            })
+        }
+    }
+    @IBAction func addCup(_ sender: AnyObject) {
+        let cupImage = UIImageView(image: UIImage(named: "RedCoffeeCup"))
+        cupImage.contentMode = .scaleAspectFit
+        
+        UIView.animate(withDuration: 0.75, animations: {
+            self.cupStackView.addArrangedSubview(cupImage)
+            self.cupStackView.layoutIfNeeded()
+        })
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
